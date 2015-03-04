22c655d1311999e670e299c8415549cb819b2f95
jiayu.qiu <jiayu.qiu@downjoy.com>
2015-03-04 18:14:06 +0800

    添加 AutoLoadTOComparator

---
+++ src/main/java/com/jarvis/cache/AutoLoadTOComparator.java
@@ -0,0 +1,39 @@
+package com.jarvis.cache;
+
+import java.util.Comparator;
+
+import com.jarvis.cache.to.AutoLoadTO;
+
+/**
+ * 排序算法：越接近过期时间，越耗时的排在最前,即： System.currentTimeMillis() - autoLoadTO.getLastLoadTime()-autoLoadTO.getExpire()*1000 值越大，排在越前
+ * autoLoadTO.getAverageUseTime() 值越大，排在越前
+ */
+public class AutoLoadTOComparator implements Comparator<AutoLoadTO> {
+
+    @Override
+    public int compare(AutoLoadTO autoLoadTO1, AutoLoadTO autoLoadTO2) {
+        if(autoLoadTO1 == null && autoLoadTO2 != null) {
+            return 1;
+        } else if(autoLoadTO1 != null && autoLoadTO2 == null) {
+            return -1;
+        } else if(autoLoadTO1 == null && autoLoadTO2 == null) {
+            return 0;
+        }
+        long now=System.currentTimeMillis();
+        long dif1=now - autoLoadTO1.getLastLoadTime() - autoLoadTO1.getExpire() * 1000;
+        long dif2=now - autoLoadTO2.getLastLoadTime() - autoLoadTO2.getExpire() * 1000;
+        if(dif1 > dif2) {
+            return -1;
+        } else if(dif1 < dif2) {
+            return 1;
+        } else {
+            if(autoLoadTO1.getAverageUseTime() > autoLoadTO2.getAverageUseTime()) {
+                return -1;
+            } else if(autoLoadTO1.getAverageUseTime() < autoLoadTO2.getAverageUseTime()) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+}
