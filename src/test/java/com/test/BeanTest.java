e0b746279bc6a2d84f559fe890e0f6bf8daa82b0
qiujiayu <jiayu.qiu@joy2u.cn>
2016-06-29 15:25:44 +0800

    改用junit 进行单元测试

--- src/test/java/com/test/BeanTest.java
+++ src/test/java/com/test/BeanTest.java
@@ -1,9 +1,11 @@
 package com.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class BeanTest {
 
