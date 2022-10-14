3887827b4d86268360f60844702f72383962d6ba
励志买套上海苏河湾大平层 <94773058+chenjjiaa@users.noreply.github.com>
2022-10-14 16:14:02 +0800

    Update SpringELParser.java

--- autoload-cache-script/autoload-cache-script-springel/src/main/java/com/jarvis/cache/script/SpringELParser.java
+++ autoload-cache-script/autoload-cache-script-springel/src/main/java/com/jarvis/cache/script/SpringELParser.java
@@ -1,7 +1,6 @@
 package com.jarvis.cache.script;
 
 import com.jarvis.cache.CacheUtil;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -92,5 +91,4 @@ public class SpringELParser extends AbstractScriptParser {
         }
         return expression.getValue(context, valueType);
     }
-
 }
